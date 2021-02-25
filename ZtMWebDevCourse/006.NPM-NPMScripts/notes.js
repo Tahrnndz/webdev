6b3f629f8046ce1bf3bd028bb8a606a16387b97d
Trey Hernandez <Tahrnndz@gmail.com>
2021-02-25 16:00:09 -0600

    starting npm study

---
+++ ZtMWebDevCourse/006.NPM-NPMScripts/notes.js
@@ -0,0 +1,38 @@
+//NPM
+// intro
+    // npm was created so devs could share js code
+    // share code and creates a knowledge base
+    // how npm works
+        // you can search for packages on npm registry
+        // contains js filel & package.js file (meta file)
+        // have small js files that do one thing really well 
+        // and compose to add functionality
+        // benefits include expertise from others
+        // but not all packages are good code & you add more 
+        // bytes to your project files
+        // three type of packages
+            // front end , back end, CLI
+    // Yarn is like NPM 
+    // Why are we learning NPM scripts
+        // Three must do things for building sites
+            // 1. HTML + CSS + JS
+            // 2. Github Repo + Git
+            // 3. Package.json file (dependency mgmt)
+// setting up NPM & package.json
+    // DL node.js (includes npm) tk: download
+    // check to see if working in terminal
+        npm -v // higher than 5.5.1
+        node -v // higher than 8.9.1 
+    // permission errors:
+        // your user doesn't have permission rights
+        // sudo npm install react
+        // or windows command
+    // npm int to start a project and creates package.json file
+    // json => file format => JavaScript object notation
+// Latest node.js & NPM
+
+// Installing & using packages
+
+// Why update packages
+
+// Exercise: Create a portfolio
