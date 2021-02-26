d519cc6022cd08ba49604b7c84d82c9e314e392e
Trey Hernandez <Tahrnndz@gmail.com>
2021-02-26 10:41:09 -0600

    updated notes for NPM section

--- ZtMWebDevCourse/006.NPM-NPMScripts/notes.js
+++ ZtMWebDevCourse/006.NPM-NPMScripts/notes.js
@@ -29,10 +29,38 @@
         // or windows command
     // npm int to start a project and creates package.json file
     // json => file format => JavaScript object notation
-// Latest node.js & NPM
-
 // Installing & using packages
+    // when we have package.json files and then use
+    // npm to dl packages
+    // live-server & lodash
+    // Can isntall things 2 ways locally & globally from NPM
+    // global means you can run on your terminal
+    // live server
+        npm install -g live-server  // can get a permission denied issue
+        sudo npm install -g live-server // fix issues
+        live-server // in background generator fold  -> loads automatically
 
-// Why update packages
+    // lodash // local packages, extension of JavaScript
+        npm install lodash 
+        // package.json adds a line for lodash now since this is local scope
+        // node modules is installed on directory as well
+        npm install -g browserify // sudo needed, used for require syntax
+        // instead of import syntax
+        var _ = require('lodash') // at top of file to use it
+        var array = [1,2,3,4,5]
+        console.log('answer:', _.without(array, 3));
 
+        browserify script.js > bundle.js  //coommand line
+                // run this ^ when we add anything with lodash
+        // browserify lets us use the require syntax so we can use stuff
+        // bundle.js includes a bunch of packages without needed them all
+        // must asses if the packages that are needed
+        // have dependencies now, and must make sure we have versions
+        // semver => symantic versioning "^4.17.4"
+        // dev dependency => packages only needed for dev and testing
 // Exercise: Create a portfolio
+    https://github.com/cobidev/simplefolio
+    https://hatchful.shopify.com/
+    https://stackoverflow.com/questions/46400443/what-is-the-difference-between-css-and-scss
+    // can use this project freely
+    clone repo
