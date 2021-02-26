1d458e128d004f33e5bc0292602c4def86572af4
Trey Hernandez <Tahrnndz@gmail.com>
2021-02-26 14:04:12 -0600

    React notes update

--- ZtMWebDevCourse/007.React/notes.js
+++ ZtMWebDevCourse/007.React/notes.js
@@ -1,7 +1,25 @@
 // 1. Intro To React
-
+    // 1. Think in Components!
+        // Lego blocks to build website
+            // Atoms => Molecules => Organisms => Templates => Pages
+        // Virtual DOM:
+            // Minimize amt of dom manipulation + too much decreases perf
+            // View == Artists, React == robot (virtual dom, auto)
+            // React takes care of issues of manipulating elements etc
+        // Has a huge ecosystem
+        // Classes Extends component, mandatory render functions,
+        //  constructor functions
 // 2. create-react-app
-
+    // https://www.npmjs.com/package/create-react-app
+    // https://github.com/facebook/create-react-app
+    // https://marketplace.visualstudio.com/items?itemName=jawandarajbir.react-vscode-extension-pack
+    1. Create Project Folder
+    2. DL React 
+    3. create-react-app
+    //np install -g create-react-app
+    //create-react-app robofriends // comand line
+    // cd into robofrineds
+    // open in sublime text
 // 3. Your First React Component
 
 // 4. Building A react app
