c8e29ec363b8c213f62ae8f6b88df6e246b02ee0
Trey Hernandez <Tahrnndz@gmail.com>
2021-02-26 11:27:06 -0600

    Starting react section

---
+++ ZtMWebDevCourse/007.React/notes.js
@@ -0,0 +1,13 @@
+// 1. Intro To React
+
+// 2. create-react-app
+
+// 3. Your First React Component
+
+// 4. Building A react app
+
+// 5. Keeping your projects up to date
+
+// 6. Error boundary in React
+
+// 7. Deploying Our React App
