bbe1d7daa53edc02d4638ab569471a971040820a
Trey Hernandez <Tahrnndz@gmail.com>
2021-03-01 11:28:58 -0600

    added cards

--- ZtMWebDevCourse/007.React/robofriends/src/index.js
+++ ZtMWebDevCourse/007.React/robofriends/src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './Hello';
+import Card from './Card';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 
-ReactDOM.render(<Hello greeting={'Hello ' + 'React Ninja'}/>, document.getElementById('root')
+ReactDOM.render(
+  <div>
+    <Card />
+    <Card />
+    <Card />
+  </div>, 
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
