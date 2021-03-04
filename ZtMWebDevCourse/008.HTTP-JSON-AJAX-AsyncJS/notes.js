9557f3e70afe6118ebca6c2b821b3ca2005ec9f5
Trey Hernandez <Tahrnndz@gmail.com>
2021-03-04 10:49:35 -0600

    Starting Backend study

--- ZtMWebDevCourse/008.HTTP-JSON-AJAX-AsyncJS/notes.js
+++ ZtMWebDevCourse/008.HTTP-JSON-AJAX-AsyncJS/notes.js
@@ -161,5 +161,83 @@ const getData1 = async function() {
 }
 
 // ### ES9 
+// object spread operator
+const animals = {
+    tiger: 25,
+    lion: 5,
+    monkey: 2
+}
+const { tiger, ...rest } = animals;
 
+const array = [1,2,3,4,5];
+function sum (a,b,c,d,e) {
+    return a + b + c + d + e;
+}
+sum (...array);
+
+function objectSpread(p1, p2, p3) {
+    console.log(p1);
+    console.log(p2);
+    console.log(p3);
+}
+
+const {tiger, lion, ...rest } = animals;
+
+objectSpread(tiger, lion, rest);
+
+// async
+const urls = [
+    'https://swapi.co/api/people/1',
+    'https://swapi.co/api/people/2',
+    'https://swapi.co/api/people/3',
+    'https://swapi.co/api/people/4'
+]
+
+Promise.all(urls.map(url => {
+    return fetch(url).then(people => people.json())
+}))
+    .then(array => {
+        console.log('1', array[0]);
+        console.log('2', array[1]);
+        console.log('3', array[2]);
+        console.log('4', array[3])
+    })
+    .catch(err => console.log('ugh fix it!', err))
+    .finally(() => console.log('extra', data));
+
+// for await of
+const urls = [
+    'https://jsonplaceholder.typicode.com/users',
+    'https://jsonplaceholder.typicode.com/posts',
+    'https://jsonplaceholder.typicode.com/albums'
+]
+
+const getData = async function() {
+    try {
+        const [ users, posts, albums ] = await Promise.all(urls.map(async function (url) {
+            const response = await fetch(url);
+            return response.json();
+        }));
+        console.log('users', users);
+        console.log('posts', posts);
+        console.log('albums', albums);
+    } catch (err) {
+        console.log('oops', err);
+    }
+}
+
+const loopThroughUrls = url =>{
+    for (url of urls) {
+        console.log(url);
+    }
+}
+
+const getData2 = async function () {
+    const arrayOfPromises = urls.map(url => fetch(url));
+    for await (let request of arrayOfPromises) {
+        const data = await request.json();
+        console.log(data);
+    }
+}
 // ### ES2020: allSettled
+// https://github.com/daumann/ECMAScript-new-features-list
---
+++ ZtMWebDevCourse/009.APIs/notes.js
@@ -0,0 +1,3 @@
+// Backend Basics
+// API's
+// Intro to API's
