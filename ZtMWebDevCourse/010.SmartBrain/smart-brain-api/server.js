5bcdac04a2be610e91b2003fbf1bb4329a758ebc
Trey Hernandez <Tahrnndz@gmail.com>
2021-05-27 12:45:08 -0500

    got backend working solo

--- ZtMWebDevCourse/010.SmartBrain/smart-brain-api/server.js
+++ ZtMWebDevCourse/010.SmartBrain/smart-brain-api/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
 
 const app = express();
 
@@ -23,6 +24,13 @@ const database = {
             entries: 0,
             joined: new Date()
         }
+    ],
+    login: [
+        {
+            id: '987',
+            hash: '',
+            email: 'John@gmail.com'
+        }
     ]
 }
 app.get('/', (req, res) => {
@@ -30,6 +38,13 @@ app.get('/', (req, res) => {
 })
 
 app.post('/signin', (req, res) => {
+    bcrypt.compare("apples", '$2a$10$xNTeGJwNWMAjyO3cjHQJAe4YiODdxTkKgh6HLBu8gEqgcea/YqBSu', function (err, res) {
+        console.log('first guess', res);
+    });
+
+    bcrypt.compare("veggies", '$2a$10$xNTeGJwNWMAjyO3cjHQJAe4YiODdxTkKgh6HLBu8gEqgcea/YqBSu', function(err, res){
+        console.log('second guess', res);
+    });
     if (req.body.email === database.users[0].email &&
         req.body.password === database.users[0].password){
             res.json('success');
@@ -40,6 +55,9 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
     const {email, name, password } = req.body;
+    bcrypt.hash(password, null, null, function (err, hash) {
+        console.log(hash);
+    });
     database.users.push(
         {
             id: '125',
@@ -82,6 +100,19 @@ app.put('/image', (req, res) => {
         res.status(400).json('not found');
     }
 })
+
+// bcrypt.hash("bacon", null, null, function (err, hash) {
+//     // store hash in your password db
+// });
+
+// // Load hash from your password DB.
+// bcrypt.compare("bacon", hash, function (err, res) {
+//     //res == true
+// });
+
+// bcrypt.compare("veggies", hash, function(err, res){
+//     // res = false
+// });
 
 app.listen(3000, () => {
     console.log('app is running on port 3000');
